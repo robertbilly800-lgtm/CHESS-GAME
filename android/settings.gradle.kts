3c794e76a124d81478f29d9f2ef9e1a566b8f718
robertbilly800-lgtm <robertbilly800@gmail.com>
2026-04-17 01:47:49 -0700

    Push latest Android configuration updates

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -19,16 +19,7 @@ pluginManagement {
 plugins {
     id("dev.flutter.flutter-gradle-plugin") version "1.0.0" apply false
     id("com.android.application") version "8.7.3" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
-}
-
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.PREFER_PROJECT)
-    repositories {
-        google()
-        mavenCentral()
-        maven { url = uri("https://storage.googleapis.com/download.flutter.io") }
-    }
+    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
 }
 
 rootProject.name = "chess_sms_app"
